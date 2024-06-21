fbc0491c5790691ad93aa94a19378a5e8f5c60c1
VaibhavAgarwal452 <vaibhavagarwal78@gmail.com>
2024-06-21 21:56:51 +0530

    added more categories

--- data.js
+++ data.js
@@ -216,211 +216,211 @@ export const PersonGrowthCategory = [
 ]
 
 export const CalmDownCategory = [
-    { id: 1, name: "Mindfulness", icon: <MaterialCommunityIcons name="brain" size={24} color="white" /> },
+    { id: 1, name: "Mindfulness", filterCategoryBy: "minimalism", icon: <MaterialCommunityIcons name="brain" size={24} color="white" /> },
     {
-        id: 2, name: "Calm", icon: <MaterialIcons name="water-drop" size={24} color="white" />
+        id: 2, name: "Calm", filterCategoryBy: "calm", icon: <MaterialIcons name="water-drop" size={24} color="white" />
     },
-    { id: 3, name: "Enjoy the moment", icon: <Feather name="sunrise" size={24} color="white" /> },
+    { id: 3, name: "Enjoy the moment", filterCategoryBy: "enjoy", icon: <Feather name="sunrise" size={24} color="white" /> },
     {
-        id: 4, name: "Hope", icon: <MaterialIcons name="child-care" size={24} color="white" />
-    },
-    {
-        id: 5, name: "Sleep", icon: <MaterialCommunityIcons name="sleep" size={24} color="white" />
+        id: 4, name: "Hope", filterCategoryBy: "hope", icon: <MaterialIcons name="child-care" size={24} color="white" />
     },
     {
-        id: 6, name: "Inner peace", icon: <Ionicons name="flower" size={24} color="white" />
+        id: 5, name: "Sleep", filterCategoryBy: "sleep", icon: <MaterialCommunityIcons name="sleep" size={24} color="white" />
     },
     {
-        id: 7, name: "Managing anxiety", icon: <MaterialCommunityIcons name="shape-circle-plus" size={24} color="white" />
+        id: 6, name: "Inner peace", filterCategoryBy: "innerPeace", icon: <Ionicons name="flower" size={24} color="white" />
     },
     {
-        id: 8, name: "Patience", icon: <FontAwesome5 name="hand-peace" size={24} color="white" />
+        id: 7, name: "Managing anxiety", filterCategoryBy: "anxiety", icon: <MaterialCommunityIcons name="shape-circle-plus" size={24} color="white" />
     },
     {
-        id: 9, name: "Stress", icon: <MaterialIcons name="cyclone" size={24} color="white" />
+        id: 8, name: "Patience", filterCategoryBy: "patience", icon: <FontAwesome5 name="hand-peace" size={24} color="white" />
     },
     {
-        id: 10, name: "Appreciation", icon: <FontAwesome5 name="hand-holding-heart" size={24} color="white" />
+        id: 9, name: "Stress", filterCategoryBy: "stress", icon: <MaterialIcons name="cyclone" size={24} color="white" />
     },
+    // {
+    //     id: 10, name: "Appreciation",filterCategoryBy: "stress", icon: <FontAwesome5 name="hand-holding-heart" size={24} color="white" />
+    // },
     {
-        id: 11, name: "Smile", icon: <Feather name="smile" size={24} color="white" />
+        id: 11, name: "Smile", filterCategoryBy: "smile", icon: <Feather name="smile" size={24} color="white" />
     }
 ]
 
 export const WorkProductiveCategry = [
-    { id: 1, name: "Success", icon: <MaterialCommunityIcons name="wifi-star" size={24} color="white" /> },
+    { id: 1, name: "Success", filterCategoryBy: "success", icon: <MaterialCommunityIcons name="wifi-star" size={24} color="white" /> },
     {
-        id: 2, name: "Money", icon: <MaterialIcons name="attach-money" size={24} color="white" />
+        id: 2, name: "Money", filterCategoryBy: "money", icon: <MaterialIcons name="attach-money" size={24} color="white" />
     },
-    { id: 3, name: "Passion", icon: <Fontisto name="fire" size={24} color="white" /> },
+    { id: 3, name: "Passion", filterCategoryBy: "passion", icon: <Fontisto name="fire" size={24} color="white" /> },
     {
-        id: 4, name: "Business", icon: <FontAwesome5 name="business-time" size={24} color="white" />
-    },
-    {
-        id: 5, name: "Leadership", icon: <MaterialIcons name="leaderboard" size={24} color="white" />
+        id: 4, name: "Business", filterCategoryBy: "business", icon: <FontAwesome5 name="business-time" size={24} color="white" />
     },
     {
-        id: 6, name: "Work", icon: <MaterialCommunityIcons name="bag-checked" size={24} color="white" />
+        id: 5, name: "Leadership", filterCategoryBy: "leadership", icon: <MaterialIcons name="leaderboard" size={24} color="white" />
     },
     {
-        id: 7, name: "Hustling", icon: <Octicons name="workflow" size={24} color="white" />
+        id: 6, name: "Work", filterCategoryBy: "work", icon: <MaterialCommunityIcons name="bag-checked" size={24} color="white" />
     },
     {
-        id: 8, name: "Self-discipline", icon: <MaterialCommunityIcons name="stairs-up" size={24} color="white" />
+        id: 7, name: "Hustling", filterCategoryBy: "hustle", icon: <Octicons name="workflow" size={24} color="white" />
     },
     {
-        id: 9, name: "Routine", icon: <Octicons name="checklist" size={24} color="white" />
+        id: 8, name: "Self-discipline", filterCategoryBy: "selfDiscipline", icon: <MaterialCommunityIcons name="stairs-up" size={24} color="white" />
+    },
+    // {
+    //     id: 9, name: "Routine", icon: <Octicons name="checklist" size={24} color="white" />
+    // },
+    // {
+    //     id: 10, name: "Productivity", icon: <Entypo name="back-in-time" size={24} color="white" />
+    // },
+    // {
+    //     id: 11, name: "Entrepreneurs", icon: <Entypo name="back-in-time" size={24} color="white" />
+    // },
+    {
+        id: 12, name: "Perseverance", filterCategoryBy: "perseverance", icon: <FontAwesome6 name="mountain" size={24} color="white" />
     },
     {
-        id: 10, name: "Productivity", icon: <Entypo name="back-in-time" size={24} color="white" />
+        id: 13, name: "Focus", filterCategoryBy: "focus", icon: <MaterialCommunityIcons name="leaf-circle-outline" size={24} color="white" />
     },
-    {
-        id: 11, name: "Entrepreneurs", icon: <Entypo name="back-in-time" size={24} color="white" />
-    },
-    {
-        id: 12, name: "Perseverance", icon: <FontAwesome6 name="mountain" size={24} color="white" />
-    },
-    {
-        id: 13, name: "Focus", icon: <MaterialCommunityIcons name="leaf-circle-outline" size={24} color="white" />
-    },
-    {
-        id: 14, name: "Graduation", icon: <FontAwesome name="graduation-cap" size={24} color="white" />
-    },
-    {
-        id: 15, name: "College", icon: <FontAwesome6 name="user-graduate" size={24} color="white" />
-    },
-    {
-        id: 16, name: "Study", icon: <Entypo name="open-book" size={24} color="white" />
-    }
+    // {
+    //     id: 14, name: "Graduation", icon: <FontAwesome name="graduation-cap" size={24} color="white" />
+    // },
+    // {
+    //     id: 15, name: "College", icon: <FontAwesome6 name="user-graduate" size={24} color="white" />
+    // },
+    // {
+    //     id: 16, name: "Study", icon: <Entypo name="open-book" size={24} color="white" />
+    // }
 ]
 
 export const InspirationCategory = [
-    { id: 1, name: "Loving kindness", icon: <MaterialCommunityIcons name="flower-pollen-outline" size={24} color="white" /> },
+    { id: 1, name: "Loving kindness", filterCategoryBy: "kindness", icon: <MaterialCommunityIcons name="flower-pollen-outline" size={24} color="white" /> },
     {
-        id: 2, name: "Funny", icon: <FontAwesome5 name="laugh-beam" size={24} color="white" />
+        id: 2, name: "Funny", filterCategoryBy: "funny", icon: <FontAwesome5 name="laugh-beam" size={24} color="white" />
     },
     {
-        id: 3, name: "Future", icon: <Entypo name="tree" size={24} color="white" />
+        id: 3, name: "Future", filterCategoryBy: "future", icon: <Entypo name="tree" size={24} color="white" />
     },
     {
-        id: 4, name: "Passion", icon: <Fontisto name="fire" size={24} color="white" />
+        id: 4, name: "Passion", filterCategoryBy: "passion", icon: <Fontisto name="fire" size={24} color="white" />
     },
     {
-        id: 5, name: "Beauty", icon: <MaterialCommunityIcons name="mirror" size={24} color="white" />
+        id: 5, name: "Beauty", filterCategoryBy: "beauty", icon: <MaterialCommunityIcons name="mirror" size={24} color="white" />
     },
     {
-        id: 6, name: "Encouraging words", icon: <FontAwesome6 name="cloud-sun-rain" size={24} color="white" />
+        id: 6, name: "Encouraging words", filterCategoryBy: "words", icon: <FontAwesome6 name="cloud-sun-rain" size={24} color="white" />
     },
     {
-        id: 7, name: "Sayings", icon: <AntDesign name="message1" size={24} color="white" />
+        id: 7, name: "Sayings", filterCategoryBy: "sayings", icon: <AntDesign name="message1" size={24} color="white" />
     },
     {
-        id: 8, name: "Art", icon: <FontAwesome5 name="paint-brush" size={24} color="white" />
+        id: 8, name: "Art", filterCategoryBy: "art", icon: <FontAwesome5 name="paint-brush" size={24} color="white" />
     },
+    // {
+    //     id: 9, name: "Feeling Blessed", icon: <MaterialCommunityIcons name="weather-sunset" size={24} color="white" />
+    // },
     {
-        id: 9, name: "Feeling Blessed", icon: <MaterialCommunityIcons name="weather-sunset" size={24} color="white" />
-    },
-    {
-        id: 10, name: "Sarcastic", icon: <Fontisto name="laughing" size={24} color="white" />
+        id: 10, name: "Sarcastic", filterCategoryBy: "funny", icon: <Fontisto name="laughing" size={24} color="white" />
     }
 ]
 
 export const RelationshipsCategory = [
-    { id: 1, name: "Forgiveness", icon: <FontAwesome6 name="user-check" size={24} color="white" /> },
+    { id: 1, name: "Forgiveness", filterCategoryBy: "forgiveness", icon: <FontAwesome6 name="user-check" size={24} color="white" /> },
     {
-        id: 2, name: "Social anxiety", icon: <FontAwesome5 name="users-slash" size={24} color="white" />
+        id: 2, name: "Social anxiety", filterCategoryBy: "anxiety", icon: <FontAwesome5 name="users-slash" size={24} color="white" />
     },
     {
-        id: 3, name: "Trust", icon: <FontAwesome name="hand-pointer-o" size={24} color="white" />
+        id: 3, name: "Trust", filterCategoryBy: "trust", icon: <FontAwesome name="hand-pointer-o" size={24} color="white" />
+    },
+    // {
+    //     id: 4, name: "Introvert", icon: <FontAwesome6 name="user-shield" size={24} color="white" />
+    // },
+    {
+        id: 5, name: "Unconditional love", filterCategoryBy: "love", icon: <FontAwesome6 name="user-shield" size={24} color="white" />
     },
     {
-        id: 4, name: "Introvert", icon: <FontAwesome6 name="user-shield" size={24} color="white" />
+        id: 6, name: "Cheating", filterCategoryBy: "lies", icon: <MaterialCommunityIcons name="emoticon-devil" size={24} color="white" />
     },
     {
-        id: 5, name: "Unconditional love", icon: <FontAwesome6 name="user-shield" size={24} color="white" />
+        id: 7, name: "Marriage", filterCategoryBy: "marriage", icon: <MaterialCommunityIcons name="human-male-female" size={24} color="white" />
     },
     {
-        id: 6, name: "Cheating", icon: <MaterialCommunityIcons name="emoticon-devil" size={24} color="white" />
+        id: 8, name: "Friendship", filterCategoryBy: "friendship", icon: <FontAwesome5 name="user-friends" size={25} color="white" />
+    },
+    // {
+    //     id: 9, name: "Parenthood", icon: <FontAwesome6 name="hands-holding-child" size={24} color="white" />
+    // },
+    // {
+    //     id: 10, name: "Loyalty", filterCategoryBy: "loyalty", icon: <FontAwesome6 name="handshake-simple" size={24} color="white" />
+    // },
+    {
+        id: 11, name: "Being single", filterCategoryBy: "loneliness", icon: <Entypo name="back-in-time" size={24} color="white" />
     },
     {
-        id: 7, name: "Marriage", icon: <MaterialCommunityIcons name="human-male-female" size={24} color="white" />
+        id: 12, name: "Distance", filterCategoryBy: "distance", icon: <MaterialIcons name="social-distance" size={24} color="white" />
+    },
+    // {
+    //     id: 13, name: "Falling in love", icon: <MaterialCommunityIcons name="heart-circle-outline" size={24} color="white" />
+    // },
+    {
+        id: 14, name: "Best friend", filterCategoryBy: "friendship", icon: <FontAwesome5 name="user-friends" size={25} color="white" />
     },
     {
-        id: 8, name: "Friendship", icon: <FontAwesome5 name="user-friends" size={25} color="white" />
+        id: 15, name: "Love", filterCategoryBy: "love", icon: <FontAwesome6 name="heart" size={24} color="white" />
     },
     {
-        id: 9, name: "Parenthood", icon: <FontAwesome6 name="hands-holding-child" size={24} color="white" />
+        id: 16, name: "Honesty", filterCategoryBy: "honesty", icon: <Ionicons name="happy" size={24} color="white" />
     },
     {
-        id: 10, name: "Loyalty", icon: <FontAwesome6 name="handshake-simple" size={24} color="white" />
+        id: 17, name: "Missing someone", filterCategoryBy: "missingSomeone", icon: <AntDesign name="user" size={24} color="white" />
+    },
+    // {
+    //     id: 18, name: "Fake people", icon: <FontAwesome name="user-secret" size={24} color="white" />
+    // },
+    {
+        id: 19, name: "Relationships", filterCategoryBy: "relationship", icon: <MaterialCommunityIcons name="relation-many-to-many" size={24} color="white" />
     },
     {
-        id: 11, name: "Being single", icon: <Entypo name="back-in-time" size={24} color="white" />
-    },
-    {
-        id: 12, name: "Distance", icon: <MaterialIcons name="social-distance" size={24} color="white" />
-    },
-    {
-        id: 13, name: "Falling in love", icon: <MaterialCommunityIcons name="heart-circle-outline" size={24} color="white" />
-    },
-    {
-        id: 14, name: "Best friend", icon: <FontAwesome5 name="user-friends" size={25} color="white" />
-    },
-    {
-        id: 15, name: "Love", icon: <FontAwesome6 name="heart" size={24} color="white" />
-    },
-    {
-        id: 16, name: "Honesty", icon: <Ionicons name="happy" size={24} color="white" />
-    },
-    {
-        id: 17, name: "Missing someone", icon: <AntDesign name="user" size={24} color="white" />
-    },
-    {
-        id: 18, name: "Fake people", icon: <FontAwesome name="user-secret" size={24} color="white" />
-    },
-    {
-        id: 19, name: "Relationships", icon: <MaterialCommunityIcons name="relation-many-to-many" size={24} color="white" />
-    },
-    {
-        id: 20, name: "Family", icon: <MaterialIcons name="family-restroom" size={24} color="white" />
+        id: 20, name: "Family", filterCategoryBy: "family", icon: <MaterialIcons name="family-restroom" size={24} color="white" />
     },
 ]
 
 export const SpiritualCategory = [
-    { id: 1, name: "Life", icon: <FontAwesome6 name="star-of-life" size={25} color="white" /> },
+    { id: 1, name: "Life", filterCategoryBy: "", icon: <FontAwesome6 name="star-of-life" size={25} color="white" /> },
     {
-        id: 2, name: "Mindfulness", icon: <MaterialCommunityIcons name="brain" size={24} color="white" />
+        id: 2, name: "Mindfulness", filterCategoryBy: "minimalism", icon: <MaterialCommunityIcons name="brain" size={24} color="white" />
+    },
+    // {
+    //     id: 3, name: "Karma", icon: <MaterialCommunityIcons name="om" size={24} color="white" />
+    // },
+    {
+        id: 4, name: "Loving kindness", filterCategoryBy: "kindness", icon: <MaterialCommunityIcons name="flower-pollen-outline" size={24} color="white" />
     },
     {
-        id: 3, name: "Karma", icon: <MaterialCommunityIcons name="om" size={24} color="white" />
+        id: 5, name: "Religion", filterCategoryBy: "religion", icon: <MaterialCommunityIcons name="compass-rose" size={24} color="white" />
     },
     {
-        id: 4, name: "Loving kindness", icon: <MaterialCommunityIcons name="flower-pollen-outline" size={24} color="white" />
+        id: 6, name: "Wisdom", filterCategoryBy: "wisdom", icon: <MaterialCommunityIcons name="owl" size={25} color="white" />
     },
     {
-        id: 5, name: "Mantras", icon: <MaterialCommunityIcons name="compass-rose" size={24} color="white" />
+        id: 7, name: "God", filterCategoryBy: "god", icon: <FontAwesome5 name="cloud-sun-rain" size={24} color="white" />
     },
     {
-        id: 6, name: "Wisdom", icon: <MaterialCommunityIcons name="owl" size={25} color="white" />
+        id: 8, name: "Hope", filterCategoryBy: "hope", icon: <MaterialIcons name="child-care" size={24} color="white" />
+    },
+    // {
+    //     id: 9, name: "Feeling Blessed", icon: <MaterialCommunityIcons name="weather-sunset" size={24} color="white" />
+    // },
+    {
+        id: 10, name: "Faith", filterCategoryBy: "faith", icon: <FontAwesome5 name="praying-hands" size={24} color="white" />
     },
     {
-        id: 7, name: "God", icon: <FontAwesome5 name="cloud-sun-rain" size={24} color="white" />
+        id: 11, name: "Philosophy", filterCategoryBy: "philosophy", icon: <FontAwesome5 name="book-reader" size={24} color="white" />
     },
-    {
-        id: 8, name: "Hope", icon: <MaterialIcons name="child-care" size={24} color="white" />
-    },
-    {
-        id: 9, name: "Feeling Blessed", icon: <MaterialCommunityIcons name="weather-sunset" size={24} color="white" />
-    },
-    {
-        id: 10, name: "Faith", icon: <FontAwesome5 name="praying-hands" size={24} color="white" />
-    },
-    {
-        id: 11, name: "Philosophy", icon: <FontAwesome5 name="book-reader" size={24} color="white" />
-    },
-    {
-        id: 12, name: "Devotions", icon: <FontAwesome6 name="hands-praying" size={24} color="white" />
-    }
+    // {
+    //     id: 12, name: "Devotions", icon: <FontAwesome6 name="hands-praying" size={24} color="white" />
+    // }
 ]
 
 export const HealthAndFitnessCategory = [
